7e5f0e50b94669ef8bc47f55ef82aab4c5676e20
“Paul <paul.huaroc.r@gmail.com>
2023-01-14 08:35:39 -0500

    Estructura terminada

---
+++ script.js
@@ -0,0 +1,34 @@
+/* function btnEncriptar() {  
+    var p1 = document.getElementById("ingreso-texto").value;
+    var nuevaStr
+
+    nuevaStr=p1.replace(/[a]/gm, "ai");
+     */
+    /* for (var i = 0; i < p1.length; i++) { /* i=insensitivo    g=global   m=multiples lineas  */
+        /* nuevaStr=p1.replace(/[a]/gm, "ai");
+        nuevaStr=p1.replace(/[e]/gm, "enter"); 
+        /* nuevaStr=p1.replace(/[i]/gm, "imes");
+        nuevaStr=p1.replace(/[o]/gm, "ober");
+        nuevaStr=p1.replace(/[u]/gm, "ufat"); 
+    } */
+    /* var nuevaStr = p1.replace("a", "ai"); */
+/*     var nuevaStr
+    for (var i = 0; i < p1.length; i++) {
+        if (p1[i] == 'a')
+        {
+            nuevaStr = p1[i].replace("a", "ai");
+        }
+    } */
+    /* `La letra "e" es convertida para "enter"`
+        `La letra "i" es convertida para "imes"`
+        `La letra "a" es convertida para "ai"`
+        `La letra "o" es convertida para "ober"`
+        `La letra "u" es convertida para "ufat" */
+/* 
+    for (var i = 0; i < 9; i++) {
+        
+        } */
+/* 
+    
+    alert(nuevaStr);
+}   */
